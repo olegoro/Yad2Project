767b14f740dc49e1a008a7202fb0d7c044505e26
olegoro <oleg.roizen@gmail.com>
2022-06-15 21:08:50 +0300

    Fixing bugs of login component in auth component.

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,4 +1,15 @@
-import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+import {
+  Component,
+  OnInit,
+  Output,
+  EventEmitter,
+  ElementRef,
+  Renderer2,
+  ViewChild,
+  HostListener,
+} from '@angular/core';
+import { NgForm, NgModel } from '@angular/forms';
+import { Location } from '@angular/common';
 
 @Component({
   selector: 'app-login',
@@ -8,11 +19,29 @@ import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 export class LoginComponent implements OnInit {
   @Output() subscribeClicked = new EventEmitter<boolean>();
 
-  constructor() {}
+  hidePassword = true;
+
+  constructor(private _location: Location) {}
 
   ngOnInit(): void {}
 
   onSubscribeClick() {
     this.subscribeClicked.emit(true);
+  }
+
+  onSubmit(form: NgForm) {
+    if (!form.valid) {
+      console.log('The form is not valid');
+      return;
+    }
+    console.log('The form is valid');
+  }
+
+  onPasswordEyeClick() {
+    this.hidePassword = !this.hidePassword;
+  }
+
+  backClicked() {
+    this._location.back();
   }
 }
